ffa586f241dc7860186b699fe31357792d724b2f
Muhammad Saeed <ms@evh98.com>
2013-11-26 19:54:30 +0000

    Initial game + GameScreen code

---
+++ Lithium/src/com/evh98/lithium/Assets.java
@@ -0,0 +1,9 @@
+package com.evh98.lithium;
+
+public class Assets {
+
+	public static void load(){
+		
+	}
+	
+}
---
+++ Lithium/src/com/evh98/lithium/screens/GameScreen.java
@@ -0,0 +1,68 @@
+package com.evh98.lithium.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL10;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.evh98.lithium.Lithium;
+
+public class GameScreen implements Screen{
+
+	Lithium game;
+	
+	OrthographicCamera camera;
+	SpriteBatch batch;
+	
+	public GameScreen(Lithium game){
+		this.game = game;
+		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(true, 1080, 1920);
+	}
+	
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0.95F, 0.95F, 0.95F, 1F);
+		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
+		
+		camera.update();
+		generalUpdate(camera);
+		batch.setProjectionMatrix(camera.combined);
+		
+		batch.begin();
+		
+		batch.end();
+	}
+
+	public void generalUpdate(OrthographicCamera camera){
+		
+	}
+	
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+	}
+
+	@Override
+	public void show() {
+	}
+
+	@Override
+	public void hide() {
+	}
+}
